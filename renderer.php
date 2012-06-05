534466af7d1e5d3f1a269792d3806624125788b8
Gabriel Mazetto <brodock@gmail.com>
2012-06-05 16:50:50 -0300

    Seleção de cursos no protótipo.

--- renderer.php
+++ renderer.php
@@ -3,7 +3,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 class tool_tutores_renderer extends plugin_renderer_base {
-    
+
     public function assign_page() {
         // Tabela
         $table = new html_table();
@@ -13,10 +13,10 @@ class tool_tutores_renderer extends plugin_renderer_base {
 
         // Dummy data
         for ($i = 1; $i <= 20; $i++) {
-          $url = new moodle_url('/admin/tool/tutores/assign.php?id='.$i);
-          $table->data[] = array(html_writer::link($url, "Grupo {$i}"), rand(40, 50), rand(1, 3));
+            $url = new moodle_url('/admin/tool/tutores/assign.php?id=' . $i);
+            $table->data[] = array(html_writer::link($url, "Grupo {$i}"), rand(40, 50), rand(1, 3));
         }
-        
+
         // Output
         $output = $this->page_header('assign');
         $output .= $this->heading('Por favor, escolha um grupo para atribuir participantes', 3);
@@ -24,7 +24,7 @@ class tool_tutores_renderer extends plugin_renderer_base {
         $output .= $this->page_footer();
         return $output;
     }
-    
+
     public function index_page() {
         // Tabela
         $table = new html_table();
@@ -34,7 +34,7 @@ class tool_tutores_renderer extends plugin_renderer_base {
 
         $controls = get_action_icon('groups.php', 'edit', get_string('edit'), get_string('edit')) . get_action_icon('#', 'delete', get_string('delete'), get_string('delete'));
         $groups_add_url = new moodle_url('/admin/tool/tutores/groups.php');
-        $add_controls = $this->heading('<a href="'. $groups_add_url. '">'.get_string('addnewgroup', 'tool_tutores').'</a>');
+        $add_controls = $this->heading('<a href="' . $groups_add_url . '">' . get_string('addnewgroup', 'tool_tutores') . '</a>');
 
         // Dummy data
         $table->data[] = array('Grupo 1', $controls);
@@ -42,7 +42,7 @@ class tool_tutores_renderer extends plugin_renderer_base {
         $table->data[] = array('Grupo 3', $controls);
         $table->data[] = array('Grupo 4', $controls);
         $table->data[] = array('Grupo 5', $controls);
-        
+
         // Output
         $output = $this->page_header('index');
         $output .= $add_controls;
@@ -53,10 +53,23 @@ class tool_tutores_renderer extends plugin_renderer_base {
     }
 
     public function page_header($currenttab) {
+        global $PAGE;
         $output = '';
         $output .= $this->header();
         $output .= $this->heading(get_string('grupos_tutoria', 'tool_tutores'));
-        
+
+        /// Print the category selector
+        $category = $PAGE->category;
+        $displaylist = array();
+        $notused = array();
+        make_categories_list($displaylist, $notused);
+
+        $select = new single_select(new moodle_url('/admin/tool/tutores/index.php'), 'category_id', $displaylist, $category->id, null, 'switchcategory');
+        $select->set_label(get_string('curso', 'tool_tutores') . ':');
+        $output .= '<div class="navbutton">';
+        $output .= $this->render($select);
+        $output .= '</div>';
+
         $toprow = array();
         $toprow[] = new tabobject('index', new moodle_url('/admin/tool/tutores/index.php'), get_string('gerenciar_tutores', 'tool_tutores'));
         $toprow[] = new tabobject('assign', new moodle_url('/admin/tool/tutores/assign.php'), get_string('designar_participantes', 'tool_tutores'));
@@ -64,10 +77,10 @@ class tool_tutores_renderer extends plugin_renderer_base {
         $tabs = array($toprow);
 
         $output .= print_tabs($tabs, $currenttab, null, null, true);
-        
+
         return $output;
     }
-    
+
     public function page_footer() {
         return $this->footer();
     }
