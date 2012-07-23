f02fae50c829fce3914eaef0be3c0b46d1e63e19
Gabriel Mazetto <gabriel@setic.ufsc.br>
2012-07-23 16:49:26 -0300

    Seletores de tutores parcialmente funcional

--- assign.php
+++ assign.php
@@ -2,69 +2,75 @@
 
 require_once('../../../config.php');
 require_once($CFG->libdir . '/adminlib.php');
-require_once($CFG->dirroot . '/' . $CFG->admin . '/roles/lib.php');
 require_once('locallib.php');
+
+$page_url = '/admin/tool/tutores/assign.php';
+$groupid = optional_param('id', null, PARAM_INT);
 
 require_login();
 require_capability('moodle/site:config', get_context_instance(CONTEXT_SYSTEM));
-admin_externalpage_setup('tooltutores');
+admin_externalpage_setup('tooltutores', '', array('curso_ufsc' => get_curso_ufsc_id(), 'id' => $groupid), $page_url);
 
 $renderer = $PAGE->get_renderer('tool_tutores');
-$groupid = optional_param('id', null, PARAM_INT);
+
 
 if (empty($groupid)) {
-  echo $renderer->assign_page();
+    echo $renderer->assign_page();
 } else {
-  
-  // Select de usuários
-  $admisselector = new admins_existing_selector();
-  $admisselector->set_extra_fields(array('username', 'email'));
 
-  $potentialadmisselector = new admins_potential_selector();
-  $potentialadmisselector->set_extra_fields(array('username', 'email'));
+    // Select de usuários
+    $admisselector = new admins_existing_selector();
+    $admisselector->set_extra_fields(array('username', 'email'));
 
-  echo $renderer->page_header('assign');
-  ?>
+    $potentialadmisselector = new tutores_potential_selector();
+    $potentialadmisselector->set_extra_fields(array('username', 'email'));
+
+    echo $renderer->page_header('assign');
+    ?>
 
 
 <div id="addadmisform">
-  <?php echo $OUTPUT->heading(get_string('definir_permissoes_curso', 'tool_tutores', 'Saúde da Família'), 3); ?>
+    <?php echo $OUTPUT->heading(get_string('definir_permissoes_curso', 'tool_tutores', 'Saúde da Família'), 3); ?>
 
-  <form id="assignform" method="post" action="<?php echo $PAGE->url ?>">
-    <div>
-      <input type="hidden" name="sesskey" value="<?php p(sesskey()); ?>" />
+    <form id="assignform" method="post" action="<?php echo $PAGE->url ?>">
+        <div>
+            <input type="hidden" name="sesskey" value="<?php p(sesskey()); ?>"/>
 
-      <table class="generaltable generalbox roleassigntable boxaligncenter" summary="">
-        <tr>
-          <td id='existingcell'>
-            <p>
-              <label for="removeselect">Membros do grupo de tutoria</label>
-            </p>
-            <?php $admisselector->display(); ?>
-          </td>
+            <table class="generaltable generalbox roleassigntable boxaligncenter" summary="">
+                <tr>
+                    <td id='existingcell'>
+                        <p>
+                            <label for="removeselect">Membros do grupo de tutoria</label>
+                        </p>
+                        <?php $admisselector->display(); ?>
+                    </td>
 
-          <td id="buttonscell">
-            <div id="addcontrols">
-              <input name="add" id="add" type="submit" value="<?php echo $OUTPUT->larrow() . '&nbsp;' . get_string('add'); ?>" title="<?php print_string('add'); ?>" /><br />
-            </div>
+                    <td id="buttonscell">
+                        <div id="addcontrols">
+                            <input name="add" id="add" type="submit"
+                                   value="<?php echo $OUTPUT->larrow() . '&nbsp;' . get_string('add'); ?>"
+                                   title="<?php print_string('add'); ?>"/><br/>
+                        </div>
 
-            <div id="removecontrols">
-              <input name="remove" id="remove" type="submit" value="<?php echo get_string('remove') . '&nbsp;' . $OUTPUT->rarrow(); ?>" title="<?php print_string('remove'); ?>" />
-            </div>
-          </td>
+                        <div id="removecontrols">
+                            <input name="remove" id="remove" type="submit"
+                                   value="<?php echo get_string('remove') . '&nbsp;' . $OUTPUT->rarrow(); ?>"
+                                   title="<?php print_string('remove'); ?>"/>
+                        </div>
+                    </td>
 
-          <td id='potentialcell'>
-            <p>
-              <label for="addselect">Possíveis usuários</label>
-            </p>
-            <?php $potentialadmisselector->display(); ?>
-          </td>
-        </tr>
-      </table>
-    </div>
-  </form>
+                    <td id='potentialcell'>
+                        <p>
+                            <label for="addselect">Possíveis usuários</label>
+                        </p>
+                        <?php $potentialadmisselector->display(); ?>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
 </div>
 
 <?php
-  echo $renderer->page_footer();
+    echo $renderer->page_footer();
 }
--- locallib.php
+++ locallib.php
@@ -2,6 +2,7 @@
 
 require_once($CFG->libdir . '/moodlelib.php');
 require_once('middlewarelib.php');
+require_once($CFG->dirroot . '/' . $CFG->admin . '/roles/lib.php');
 
 function create_grupo_tutoria($curso_ufsc, $nome) {
 	$middleware = Academico::singleton();
@@ -51,5 +52,68 @@ function update_grupo_tutoria($curso_ufsc, $grupo, $nome) {
 }
 
 function redirect_to_gerenciar_tutores() {
-	redirect(new moodle_url('/admin/tool/tutores/index.php', array('curso_ufsc' => get_curso_ufsc_id())));
+    redirect(new moodle_url('/admin/tool/tutores/index.php', array('curso_ufsc' => get_curso_ufsc_id())));
 }
+
+class tutores_potential_selector extends user_selector_base {
+
+    /**
+     * @param string $name control name
+     * @param array $options should have two elements with keys groupid and courseid.
+     */
+    public function __construct($tutores_ids = null) {
+        global $CFG, $USER;
+
+        // Precisamos sobrescrever o arquivo Javascript responsavel pelo seletor ajax
+        self::$jsmodule['fullpath'] = '/admin/tool/tutores/selector/module.js';
+
+        parent::__construct('addselect', array('multiselect' => false, 'exclude' => $tutores_ids));
+    }
+
+    public function find_users($search) {
+        global $CFG, $DB;
+
+        $papeis_tutores = '46';
+
+        list($wherecondition, $params) = $this->search_sql($search, '');
+
+        $fields = 'SELECT ' . $this->required_fields_sql('');
+        $countfields = 'SELECT COUNT(1)';
+
+        $sql = " FROM {user}
+                WHERE $wherecondition AND mnethostid = :localmnet";
+        $order = ' ORDER BY lastname ASC, firstname ASC';
+
+        $params['localmnet'] = $CFG->mnet_localhost_id; // it could be dangerous to make remote users admins and also this could lead to other problems
+        // Check to see if there are too many to show sensibly.
+        if (!$this->is_validating()) {
+            $potentialcount = $DB->count_records_sql($countfields . $sql, $params);
+            if ($potentialcount > 100) {
+                return $this->too_many_results($search, $potentialcount);
+            }
+        }
+
+        $availableusers = $DB->get_records_sql($fields . $sql . $order, $params);
+
+        if (empty($availableusers)) {
+            return array();
+        }
+
+        if ($search) {
+            $groupname = get_string('potusersmatching', 'role', $search);
+        } else {
+            $groupname = get_string('potusers', 'role');
+        }
+
+        return array($groupname => $availableusers);
+    }
+
+    protected function get_options() {
+        global $CFG;
+        $options = parent::get_options();
+        $options['file'] = $CFG->admin . '/roles/lib.php';
+        return $options;
+    }
+
+}
+
