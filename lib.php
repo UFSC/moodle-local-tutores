a7b5bb725ce6ec452c128f6ad790957de1f1c563
Gabriel Mazetto <brodock@gmail.com>
2013-02-28 17:08:45 -0300

    Correção de código para adequar a mudanças na API do moodle

--- lib.php
+++ lib.php
@@ -12,8 +12,10 @@ function local_tutores_extends_settings_navigation(navigation_node $navigation) 
     global $PAGE;
 
     if (is_a($PAGE->context, 'context_coursecat') && has_capability('local/tutores:manage' ,$PAGE->context)) {
-        $category_node = $navigation->get(0);
+        $category_node = $navigation->get('categorysettings');
 
+        // Se por algum motivo a chave for alterada em uma nova versão do Moodle,
+        // Não quebrar o código.
         if ($category_node) {
             $grupostutoria_node = $category_node->add(get_string('grupos_tutoria', 'local_tutores'), null, navigation_node::TYPE_CONTAINER);
             $grupostutoria_node->add(get_string('manage_groups', 'local_tutores'), new moodle_url('/local/tutores/index.php', array('categoryid' => $PAGE->context->instanceid)));
