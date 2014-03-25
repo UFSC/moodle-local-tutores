ebeea943277ad0f8ab24bf3f3c18035b18761df5
Gabriel Mazetto <brodock@gmail.com>
2014-03-25 18:50:24 -0300

    Suporte a Moodle 2.6

--- version.php
+++ version.php
@@ -2,8 +2,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2013020400; // The current plugin version (Date: YYYYMMDDXX)
-$plugin->requires  = 2012062501; // Requires this Moodle version
+$plugin->version   = 2014032500; // The current plugin version (Date: YYYYMMDDXX)
+$plugin->requires  = 2013110500; // Requires this Moodle version
 $plugin->component = 'local_tutores'; // Full name of the plugin (used for diagnostics)
 $plugin->dependencies = array('local_academico' => 2012081700);
 
