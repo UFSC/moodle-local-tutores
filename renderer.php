f5458898f58b2f9afb9d2dee5aad6b8e8a39d0ef
Gabriel Mazetto <brodock@gmail.com>
2014-09-18 18:28:23 -0300

    WIP: Algumas correções na interface e troca de mensagens
    Ainda é necessário corrigir a lógica para que ela passe a funcionar
    na nova estrutura sem “curso_ufsc_id”

--- renderer.php
+++ renderer.php
@@ -10,18 +10,18 @@ class local_tutores_renderer extends plugin_renderer_base {
         parent::__construct($page, $target);
 
         $this->categoryid = required_param('categoryid', PARAM_INT);
-        $this->curso_ativo = get_curso_ufsc_id();
     }
 
-    public function create_confirmation($return_url, $confirm_url, $curso_ufsc) {
+    public function create_confirmation($return_url, $confirm_url, $categoryid) {
         global $OUTPUT;
 
+        $category = coursecat::get($categoryid);
         $formcontinue = new single_button($confirm_url, get_string('yes'));
         $formcancel = new single_button($return_url, get_string('no'), 'get');
 
         $output = $this->page_header('index');
         $output .= $OUTPUT->heading(get_string('create_confirmation', 'local_tutores'));
-        $output .= $OUTPUT->confirm(get_string('confirm_relationship_creation', 'local_tutores', $curso_ufsc), $formcontinue, $formcancel);
+        $output .= $OUTPUT->confirm(get_string('confirm_relationship_creation', 'local_tutores', $category->name), $formcontinue, $formcancel);
 
         return $output;
     }
