39dae38265f6ec2315af3083e40f37cc2359bb38
Gabriel Mazetto <brodock@gmail.com>
2014-09-18 18:10:05 -0300

    Versão 2014091800

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014081104; // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2014091800; // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013111803.02; // Requires this Moodle version
 $plugin->component = 'local_tutores'; // Full name of the plugin (used for diagnostics)
 $plugin->dependencies = array('local_academico' => 2012081700);
