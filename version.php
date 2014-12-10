8e2c234becbdc1230654bef9de0539386f7fafdb
Roberto Silvino <robertosilvino@gmail.com>
2014-12-10 17:06:19 -0200

    alteração da versão da ferramenta

--- version.php
+++ version.php
@@ -2,7 +2,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014091801; // The current plugin version (Date: YYYYMMDDXX)
+$plugin->version   = 2014121001; // The current plugin version (Date: YYYYMMDDXX)
 $plugin->requires  = 2013111803.02; // Requires this Moodle version
 $plugin->component = 'local_tutores'; // Full name of the plugin (used for diagnostics)
 
